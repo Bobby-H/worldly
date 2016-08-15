4c7682fe47534c3e306c2de21f43c7dbcaaca147
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-15 02:00:59 -0400

    added images and travel_pics loop to index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @homeland = 'Charlotte, NC, USA'
     @countries = ['Chile', 'Bolivia', 'Croatia']
+    @travel_pics = ['Chile.jpg', 'Bolivia.jpg', 'Croatia.jpg']
   end
 
   def about
